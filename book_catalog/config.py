31ed2874c1dd9d3790fd1d52f658ad38aa3d00c3
Niwanthi Edirisinghe <niwanthi.e@wemakeplatforms.io>
2024-09-12 20:55:17 +1000

    add deployment files

--- book_catalog/config.py
+++ book_catalog/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("DATABASE_URL","postgresql://book_catalog_0sml_user:9T5Pxbby74ZAowwwHWibKE8gkHbcuww9@dpg-crhaf7l6l47c73c5512g-a.singapore-postgres.render.com/book_catalog_0sml")
 
 settings = Settings()
